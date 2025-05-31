5be320912b287e948a7c402a8f49ada6269a236c
-m <villamorjohnnoel@gmail.com>
2025-06-01 07:23:32 +0800

    fix(be-User): fix the error handling in routes for better error message feedback.

--- backend/app/routes/user.py
+++ backend/app/routes/user.py
@@ -15,37 +15,29 @@ router = APIRouter(
 
 @router.post('/register', status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserCreate, db: Session =Depends(get_db)):
-  try:
-    existing_user = db.query(User).filter(User.email == user.email).first()
+  existing_user = db.query(User).filter(User.email == user.email).first()
 
-    if existing_user:
-      raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
-    
-    hashed_password = hash_password(user.password)
-    user.password = hashed_password
+  if existing_user:
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+  
+  hashed_password = hash_password(user.password)
+  user.password = hashed_password
 
-    new_user = User(**user.model_dump())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+  new_user = User(**user.model_dump())
+  db.add(new_user)
+  db.commit()
+  db.refresh(new_user)
 
-    return UserRead.model_validate(new_user).model_dump()
-  except Exception as e:
-    print(f"Error creating user: {e}", flush=True)
-    raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error")
+  return UserRead.model_validate(new_user).model_dump()
   
 @router.get('/users/{user_id}', response_model=UserRead, status_code=status.HTTP_200_OK)
 def get_user(user_id: int, db: Session = Depends(get_db)):
-  try:
-    user = db.query(User).filter(User.id == user_id).first()
-    
-    if not user:
-      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-    
-    return UserRead.model_validate(user).model_dump()
-  except Exception as e:
-    print(f"Error retrieving user: {e}", flush=True)
-    raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error")
+  user = db.query(User).filter(User.id == user_id).first()
+  
+  if not user:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+  
+  return UserRead.model_validate(user).model_dump()
 
 @router.post("/login", response_model=TokenSchema, status_code=status.HTTP_200_OK)
 async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
