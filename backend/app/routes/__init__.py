6159b8dc1d4c0f3baed6ab7eba2d77518666bec6
-m <villamorjohnnoel@gmail.com>
2025-06-01 07:24:50 +0800

    chore(migrations): migrate the latest changes in models.

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from .user import router as user_router
+from .blog import router as blog_router
 
 def register_routes(app: FastAPI) -> None:
   app.include_router(user_router)
+  app.include_router(blog_router)
