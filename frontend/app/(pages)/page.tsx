b3f0170763d0d0cf36f22e8f518333d96a8d8ebc
-m <villamorjohnnoel@gmail.com>
2025-06-01 23:19:38 +0800

    chore(fe-home): include a text if no blogs retreived.

--- frontend/app/(pages)/page.tsx
+++ frontend/app/(pages)/page.tsx
@@ -7,9 +7,14 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ [
   const limit = (await searchParams).limit || '10';
 
   const blogPosts: PaginatedResponse<BlogPost> = await getBlogPosts(Number(page), Number(limit));
+
+  if (!blogPosts || !blogPosts.data || blogPosts.data.length === 0) {
+    return <div className='container mx-auto px-4 py-8 text-center'>No blog posts found.</div>;
+  }
+
   return (
     <div>
-       <div className='container mx-auto px-4 py-8'>
+      <div className='container mx-auto px-4 py-8'>
         <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
           {blogPosts.data.map((post) => (
             <BlogCard key={post.id} post={post} />
