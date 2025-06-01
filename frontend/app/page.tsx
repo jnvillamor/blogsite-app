798f73948d59307e760c05e9e55a2e7f5165df18
-m <villamorjohnnoel@gmail.com>
2025-06-01 18:38:54 +0800

    fix(fe-home): refactor homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,14 +4,9 @@ import Link from 'next/link';
 import { getBlogPosts } from './api/blog';
 import { BlogPost, PaginatedResponse } from '@/types';
 
-export default async function Home({ searchParams }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }> }) {
-  const page = (await searchParams).page || '1';
-  const limit = (await searchParams).limit || '10';
-
-  const blogPosts: PaginatedResponse<BlogPost> = await getBlogPosts(Number(page), Number(limit));
+async function Blogs({ blogPosts }: { blogPosts: PaginatedResponse<BlogPost> }) {
   return (
     <>
-      <Header />
       <div className='container mx-auto px-4 py-8'>
         <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
           {blogPosts.data.map((post) => (
@@ -34,6 +29,19 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ [
           ))}
         </div>
       </div>
+    </>
+  );
+}
+
+export default async function Home({ searchParams }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }> }) {
+  const page = (await searchParams).page || '1';
+  const limit = (await searchParams).limit || '10';
+
+  const blogPosts: PaginatedResponse<BlogPost> = await getBlogPosts(Number(page), Number(limit));
+  return (
+    <>
+      <Header />
+      <Blogs blogPosts={blogPosts} />
     </>
   );
 }
